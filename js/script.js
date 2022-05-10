0903eeecb9f9f44b01eab8c183fe302cf7bd3103
francopirol <pirolofip@gmail.com>
2022-05-10 10:36:43 -0300

    imagenes

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let frutas = ["banana", "manzana", "naranja","pera", "uva","sandia","mandarina"];
+let carnes = ["carne", "pollo", "cerdo, pescado"];
+let galletas = ["oreos", "merengadas", "terrabusi, sonrisas"];
+let lacteos = ["leche", "manteca", "queso"];
