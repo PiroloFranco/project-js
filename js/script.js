85ef67794a63ae68b092a9b94f797332221e88e4
francopirol <pirolofip@gmail.com>
2022-05-12 21:12:44 -0300

    segunda version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-let frutas = ["banana", "manzana", "naranja","pera", "uva","sandia","mandarina"];
-let carnes = ["carne", "pollo", "cerdo, pescado"];
-let galletas = ["oreos", "merengadas", "terrabusi, sonrisas"];
-let lacteos = ["leche", "manteca", "queso"];
+let frutas = {
+    id: 0,
+    nombre: "Frutas",
+    variedes: ["banana", "manzana", "naranja","pera", "uva","sandia","mandarina"],
+    precios:[50, 30, 70, 90, 20, 100, 60]
+};
+let carnes = {
+    id: 1,
+    nombre: "carnes",
+    variedes: ["carne", "pollo", "cerdo, pescado"],
+    precios:[100, 70, 80, 110]
+};
+let galletas = {
+    id: 2,
+    nombre: "galletas",
+    variedes: ["oreos", "merengadas", "terrabusi, sonrisas"],
+    precios:[100, 70, 40, 90]
+};
+let lacteos = {
+    id: 3,
+    nombre: "lacteos",
+    variedes: ["leche", "manteca", "queso"],
+    precios:[50 ,80, 100]
+};
+
+setAttribute(frutas,"banana", "manzana", "naranja","pera", "uva","sandia","mandarina" )
+
+setAttribute(carnes,"carne", "pollo", "cerdo, pescado" )
+
+setAttribute(galletas,"oreos", "merengadas", "terrabusi, sonrisas" )
+
+setAttribute(lacteos,"leche", "manteca", "queso" )
